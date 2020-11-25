431b6f5f399ebab52bed97f469b02706b3bce699
krystek77 <krystian.wrona77@gmail.com>
2020-11-25 11:50:25 +0100

    Create basic application folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+const HomePage = React.lazy(() => import('./pages/home'));
 
 export const App = () => {
-  return <h1>App page</h1>;
+  return (
+    <React.Suspense fallback={<div>Loading page</div>}>
+      <HomePage />
+    </React.Suspense>
+  );
 };
 
 export default App;
---
+++ src/components/layout/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Layout: React.FC = ({ children }) => {
+  return <React.Fragment>{children}</React.Fragment>;
+};
+
+export default Layout;
