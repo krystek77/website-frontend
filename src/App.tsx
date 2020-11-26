9fd75266b7de42bb35ebd3fc06d7d1f4f16c40e9
krystek77 <krystian.wrona77@gmail.com>
2020-11-26 09:37:40 +0100

    Wrap all components by Router  Please enter the commit message for your changes. Lines starting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 const HomePage = React.lazy(() => import('./pages/home'));
 
 export const App = () => {
   return (
-    <React.Suspense fallback={<div>Loading page</div>}>
-      <HomePage />
-    </React.Suspense>
+    <Router>
+      <React.Suspense fallback={<div>Loading page</div>}>
+        <HomePage />
+      </React.Suspense>
+    </Router>
   );
 };
 
