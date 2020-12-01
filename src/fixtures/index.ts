f610bccb1d9a2a283b765c4a4cafd38b83f9c6c9
krystek77 <krystian.wrona77@gmail.com>
2020-12-01 11:32:40 +0100

    Save slider data structire from strapi
    I have to adjust my components to that data structure

--- src/fixtures/index.ts
+++ src/fixtures/index.ts
@@ -2,4 +2,5 @@ import { pageLinks } from './pageLinks';
 import { userLinks } from './userLinks';
 import { socialLinks } from './socialLinks';
 import { slider } from './slider';
-export { pageLinks, userLinks, socialLinks, slider };
+import { strapi_slider } from './strapi_slider';
+export { pageLinks, userLinks, socialLinks, slider, strapi_slider };
