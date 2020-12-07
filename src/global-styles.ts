2b866e35fd041a73f56f8c4ce101d963a54b944f
krystek77 <krystian.wrona77@gmail.com>
2020-12-07 13:43:31 +0100

    Update styles of title elements

--- src/global-styles.ts
+++ src/global-styles.ts
@@ -73,10 +73,12 @@ export const GlobalStyles = createGlobalStyle<{ theme: ThemeType }>`
         text-transform:uppercase;
     }
     h2 {
-        font-size:2.8rem;
+        font-size:2.4rem;
+        text-transform:uppercase;
+        font-weight:500;
     }
     h3 {
-        font-size:2.4rem;
+        font-size:2.2rem;
     }
     h4 {
         font-size:2rem;
