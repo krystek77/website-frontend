d208b5192f1c6230bd57d0e8a410e5bcb887d021
krystek77 <krystian.wrona77@gmail.com>
2020-12-07 10:08:40 +0100

    Display links to other businesses on business page

--- src/pages/businessDetails.tsx
+++ src/pages/businessDetails.tsx
@@ -1,37 +1,22 @@
 import React from 'react';
+import { NavLink as Link } from 'react-router-dom';
+import { ROUTES } from '../constants';
+import { useBusiness } from '../hooks';
 import { Paragraph } from '../components';
 import { withRouter } from 'react-router-dom';
 
-type Item = {
-  id: number;
-  title: string;
-  Description: string;
-  image: { url: string; alternativeText: string };
-  slug: string;
-};
-
 export const BusinessDetailsPage = withRouter(({ ...restProps }) => {
+  const { businesses } = useBusiness();
   const {
     match: {
       params: { slug },
     },
   } = restProps;
-  const [businessDetails, setBusinessDetails] = React.useState<Item | null>(
-    null
-  );
-  React.useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const responseData = await fetch(`/businesses/${slug}`);
-        const data = await responseData.json();
-        setBusinessDetails(data);
-      } catch (error) {}
-    };
-    fetchData();
-    return () => {};
-  }, [slug]);
 
-  console.log(businessDetails);
+  let business = null;
+  if (businesses && businesses.length > 0) {
+    business = businesses.find((item) => item.slug === slug);
+  }
 
   return (
     <React.Fragment>
@@ -50,24 +35,44 @@ export const BusinessDetailsPage = withRouter(({ ...restProps }) => {
       >
         <h1>Twój biznes</h1>
       </header>
-      <main>
-        <h2>Gościnność - slug: {slug}</h2>
-        <Paragraph>
-          As experts in healthcare laundry solutions, Primus offers a uniquely
-          comprehensive and innovative range of hygienic barrier
-          washer-extractors to simplify and optimize the setting up of your
-          laundry. All our machines are designed with the highest quality
-          components to ensure a long lifespan and optimized capacity in
-          minimized space.
-        </Paragraph>
-        <Paragraph>
-          Primus machines are a beacon of efficiency and outstanding hygiene to
-          ensure user satisfaction. The patented technology developed by Primus,
-          specifically for the healthcare industry, provides exquisite
-          performance while being user-friendly, and facilitating easy control,
-          monitoring and analysis.
-        </Paragraph>
-      </main>
+      {business && (
+        <React.Fragment>
+          <main>
+            <h2>{business.title}</h2>
+            <Paragraph>
+              As experts in healthcare laundry solutions, Primus offers a
+              uniquely comprehensive and innovative range of hygienic barrier
+              washer-extractors to simplify and optimize the setting up of your
+              laundry. All our machines are designed with the highest quality
+              components to ensure a long lifespan and optimized capacity in
+              minimized space.
+            </Paragraph>
+            <Paragraph>
+              Primus machines are a beacon of efficiency and outstanding hygiene
+              to ensure user satisfaction. The patented technology developed by
+              Primus, specifically for the healthcare industry, provides
+              exquisite performance while being user-friendly, and facilitating
+              easy control, monitoring and analysis.
+            </Paragraph>
+          </main>
+          <aside>
+            <h3>
+              <Link to={ROUTES.YOUR_BUSINESS}>Twój bizness</Link>
+            </h3>
+            <ul>
+              {businesses.map((item) => {
+                return (
+                  <li key={item.id}>
+                    <Link to={`${ROUTES.YOUR_BUSINESS}/${item.slug}`}>
+                      {item.title}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </aside>
+        </React.Fragment>
+      )}
     </React.Fragment>
   );
 });
