58a13066515d92078eea71b40f81ef0a0b4d849b
krystek77 <krystian.wrona77@gmail.com>
2020-12-12 15:42:56 +0100

    Change style of line title

--- src/components/lineTitle/styles/lineTitle.ts
+++ src/components/lineTitle/styles/lineTitle.ts
@@ -2,8 +2,9 @@ import styled from 'styled-components/macro';
 export const Container = styled.div``;
 export const Inner = styled.div`
   display: flex;
-  justify-content: flex-start;
-  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
   flex-wrap: wrap;
   ${({ theme }) => `margin-bottom:${theme.margin * 2}rem;`}
 `;
@@ -11,15 +12,17 @@ export const AttributeWrapper = styled.div`
   ${({ theme }) => ``}
 `;
 export const Line = styled.span`
+  display: block;
   font-size: 2.6rem;
   text-transform: capitalize;
   border-radius: 0.4rem;
   ${({ theme }) =>
-    `margin-right:${theme.margin * 1}rem;
-     color:${theme.colors.light};
-     font-weight:${theme.bold_font};
-     background-color:${theme.colors.primary};
-     padding:${theme.padding}rem;
+    ` margin-right:${theme.margin * 1}rem;
+      margin-bottom:${theme.margin * 1}rem;
+      color:${theme.colors.light};
+      font-weight:${theme.bold_font};
+      background-color:${theme.colors.dark};
+      padding:${theme.padding}rem;
      `}
 `;
 export const Title = styled.h3`
@@ -27,6 +30,7 @@ export const Title = styled.h3`
   /* white-space: nowrap; */
   text-transform: uppercase;
   margin-bottom: 0;
+  line-height: 1;
   ${({ theme }) =>
     `
   color:${theme.colors.dark};
