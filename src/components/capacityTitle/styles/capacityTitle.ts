25902a7764f3a496258d711b7737c7e2470c61a7
krystek77 <krystian.wrona77@gmail.com>
2020-12-12 11:47:48 +0100

    Change styles of capacity title component

--- src/components/capacityTitle/styles/capacityTitle.ts
+++ src/components/capacityTitle/styles/capacityTitle.ts
@@ -1,73 +1,42 @@
 import styled from 'styled-components/macro';
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding: 1rem 1.5rem;
+`;
 export const Inner = styled.div`
   display: flex;
-  justify-content: flex-start;
+  justify-content: flex-end;
   align-items: center;
-  flex-wrap: wrap;
-  ${({ theme }) =>
-    `margin-bottom:${theme.margin * 6}rem;
-     margin-top:${theme.margin * 6}rem;
-    `}
 `;
-export const AttributeWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  ${({ theme }) => ``}
-`;
-export const Name = styled.span`
-  text-transform: capitalize;
-  font-size: 2.2rem;
-  font-weight: 500;
-  ${({ theme }) =>
-    `margin-right:${theme.margin * 1}rem;
-     color:${theme.colors.primary};`}
-`;
+export const AttributeWrapper = styled.div``;
+export const Name = styled.span``;
 export const Value = styled.span`
-  position: relative;
-  font-weight: 700;
-
-  border-radius: 50%;
-  font-size: 2.5rem;
-  width: 100px;
-  height: 100px;
+  flex-shrink: 0;
+  width: 120px;
+  height: 120px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  ${({ theme }) =>
-    `margin-right:${theme.margin * 2}rem;
-    background-color: ${theme.colors.dark_secondary};
-     color:${theme.colors.light};`}
-  &:before {
-    position: absolute;
-    z-index: -1;
-    display: block;
-    content: '';
-    width: 120px;
-    height: 120px;
-    top: -10px;
-    left: -10px;
-    border-radius: 50%;
-    ${({ theme }) =>
-      `border:1px solid ${theme.colors.dark_secondary};
-     `}
-  }
+  border-radius: 50%;
+  font-size: 3rem;
+  box-shadow: 0px 0px 0px 4px rgba(255, 255, 255, 1);
+  ${({ theme }) => `
+    background-color:${theme.colors.dark_secondary};
+    color:${theme.colors.light};
+    font-weight:${theme.bold_font};
+  `}
 `;
 export const Title = styled.span`
-  font-size: 2.2rem;
-  white-space: nowrap;
-  font-weight: 500;
-  text-transform: capitalize;
-  padding: 0.8rem;
-  position: relative;
-  z-index: -1;
-  border-top-right-radius: 0.6rem;
-  border-bottom-right-radius: 0.6rem;
+  font-size: 1.8rem;
+  border-top-left-radius: 0.6rem;
+  border-bottom-left-radius: 0.6rem;
+
   ${({ theme }) => `
-   color:${theme.colors.light};
-   background-color:${theme.colors.primary};
-   margin-left: -${theme.margin * 4}rem;
-   padding-left:${theme.padding * 4}rem;
-  `};
+    background-color:${theme.colors.primary};
+    padding:${theme.padding * 3}rem ${theme.padding * 2}rem;
+    padding-right: ${theme.padding * 4}rem;
+    margin-right: -${theme.padding * 1}rem;
+    color:${theme.colors.light};
+    font-weight:${theme.bold_font};
+  `}
 `;
